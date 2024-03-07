7f237bc8d86d07140d4c13b523df2224bdd34b45
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2024-03-07 18:49:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as reqs_file:
 
 setup(
     name =  'sireneta',
-    description = 'Response Network Analysis: A package to study complex networks in the light of canonical propagation models.',
+    description = 'A library to study complex networks in the light of canonical propagation models.',
     url =  'https://github.com/mb-BCA/SiReNetA',
     version =  '0.1.dev1',
     license =  'Apache License 2.0',
