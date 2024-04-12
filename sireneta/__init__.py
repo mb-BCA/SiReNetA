1eda92a24671d51e03a492f98130ce6c62acb20c
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2024-04-12 17:57:11 +0200

    Set new version ID for the branch

--- sireneta/__init__.py
+++ sireneta/__init__.py
@@ -123,8 +123,8 @@ __author__ = "Gorka Zamora-Lopez and Matthieu Gilson"
 __email__ = "gorka@Zamora-Lopez.xyz"
 __copyright__ = "Copyright 2024"
 __license__ = "Apache License version 2.0"
-__version__ = "0.1.dev2"
-__update__ = "14/03/2024"
+__version__ = "1.0.0.dev1"
+__update__ = "12/04/2024"
 
 
 
