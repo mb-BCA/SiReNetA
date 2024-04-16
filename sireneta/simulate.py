d8d628584d22e0406f685bff4cd31f96597bc3f3
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2024-04-16 23:49:19 +0200

    Splitted MOU() simulation from LeakyCascade()

--- sireneta/simulate.py
+++ sireneta/simulate.py
@@ -34,6 +34,8 @@ ContCascade
     Simulates temporal evolution of the nodes for the continuous cascade.
 LeakyCascade
     Simulates temporal evolution of the nodes for the leaky-cascade model.
+MOU
+    Simulates temporal evolution for the multivariate Ornstein-Uhlenbeck.
 ContDiffusion
     Simulates temporal evolution of the nodes for the simple diffustion model.
 
@@ -251,7 +253,7 @@ def ContCascade(con, X0=1.0, noise=None, tmax=10, timestep=0.01):
 
     return Xdot
 
-def LeakyCascade(con, X0=1.0, tau=1.0, noise=None, tmax=10, timestep=0.01):
+def LeakyCascade(con, X0=1.0, tau=1.0, tmax=10, timestep=0.01):
     """Simulates temporal evolution of the nodes for the leaky-cascade model.
 
     It solves the differential equation for the linear propagation model of
@@ -259,6 +261,88 @@ def LeakyCascade(con, X0=1.0, tau=1.0, noise=None, tmax=10, timestep=0.01):
     a fraction of the activity.
 
             xdot(t) = - x(t) / tau + A x(t).
+
+     With λmax being the largest eigenvalue of the (positive definite) matrix A,
+     - if tau < tau_max = 1 / λmax, then the leakage term dominates in the long
+     time and the solutions for all nodes converge to zero.
+     - If tau = tau_max, all nodes converge to x_i(t) = 1.
+     - And, if tau < tau_max, then time-courses xdot(t) grow exponentially fast.
+
+    Parameters
+    ----------
+    con : ndarray (2d) of shape (N,N).
+        The connectivity matrix of the network.
+    X0 : scalar or ndarray (1d) of length N, optional
+        Initial values of the nodes at time t = 0. If scalar value is entered,
+        `X0 = c`, all nodes are initialised as `X0[i] = c` (same initial conditions).
+        Default value, `X0 = 1.0`. If a 1d-array is entered, each node i is
+        assigned initial value `X0[i]`.
+    tau : real value or ndarray (1d) of length N, optional
+        The decay time-constants of the nodes. If a scalar value is entered,
+        `tau = c`, then all nodes will be assigned the same value `tau[j] = 2`
+        (identical nodes). If an 1d-array is entered, each node i is assigned
+        decay time-constant `tau[i]`. Default `tau = 1.0` is probably too large
+        for most real networks and will diverge. If so, enter a `tau` smaller
+        than the spectral diameter (λ_max) of `con`.
+    tmax : scalar, optional
+        Duration of the simulation, arbitrary time units.
+    timestep : scalar, optional
+        Time-step of the numerical integration.
+
+    Returns
+    -------
+    Xdot : ndarray (2d) of shape (nt,N)
+        Time-courses of the N nodes.
+
+    NOTE
+    ----
+    Total number of integration steps (samples) is `nt = round(tmax*timestep) + 1`.
+    - Simulation runs from t=0 to t=tmax.
+    - Integration goes from it=0 to it=nt, with `Xdot[0] = X0`.
+    - The sampled time points are `tpoints = np.arange(0,tmax+timestep,timestep)`
+    """
+    # 0) HANDLE AND CHECK THE INPUTS
+    io_helpers.validate_con(con)
+    N = len(con)
+    X0 = io_helpers.validate_X0(X0, N)
+    tau = io_helpers.validate_tau(tau, N)
+
+    if tmax <= 0.0: raise ValueError("'tmax' must be positive")
+    if timestep <= 0.0: raise ValueError( "'timestep' must be positive")
+    if timestep >= tmax: raise ValueError("'timestep' must be smaller than 'tmax'")
+
+    # Ensure all arrays are of same dtype (float64)
+    if con.dtype != np.float64:     con = con.astype(np.float64)
+    if X0.dtype != np.float64:      X0 = X0.astype(np.float64)
+    if tau.dtype != np.float64:     tau = tau.astype(np.float64)
+
+    # 1) PREPARE FOR THE SIMULATION
+    # Conver the time-constants into decay rations
+    alphas = 1./tau
+    # Initialise the output array
+    nt = round(tmax / timestep) + 1
+    Xdot = np.zeros((nt, N), np.float64, order='C')
+    # Enter the initial conditions
+    Xdot[0] = X0
+
+    # 2) RUN THE SIMULATION
+    for t in range(1,nt):
+        Xpre = Xdot[t-1]
+        # Calculate the input to nodes due to couplings
+        xcoup = np.dot(con,Xpre)
+        # Integration step
+        Xdot[t] = Xpre + timestep * ( -alphas * Xpre + xcoup )
+
+    return Xdot
+
+def MOU(con, X0=1.0, tau=1.0, noise=1.0, tmax=10, timestep=0.01):
+    """Simulates temporal evolution for the multivariate Ornstein-Uhlenbeck.
+
+    It solves the differential equation for the linear propagation model of
+    nodes coupled via connectivity matrix A, and a dissipative term leaking
+    a fraction of the activity under Gaussian noise D added at every node.
+
+            xdot(t) = - x(t) / tau + A x(t) + D(t)
 
      With λmax being the largest eigenvalue of the (positive definite) matrix A,
      - if tau < tau_max = 1 / λmax, then the leakage term dominates in the long
@@ -320,11 +404,10 @@ def LeakyCascade(con, X0=1.0, tau=1.0, noise=None, tmax=10, timestep=0.01):
     if con.dtype != np.float64:     con = con.astype(np.float64)
     if X0.dtype != np.float64:      X0 = X0.astype(np.float64)
     if tau.dtype != np.float64:     tau = tau.astype(np.float64)
-    if noise is not None and noise.dtype != np.float64:
-        noise = noise.astype(np.float64)
+    if noise.dtype != np.float64:   noise = noise.astype(np.float64)
 
     # 1) PREPARE FOR THE SIMULATION
-    # Conver the time-constants into decay rations
+    # Convert the time-constants into decay rations
     alphas = 1./tau
     # Initialise the output array
     nt = round(tmax / timestep) + 1
@@ -333,20 +416,12 @@ def LeakyCascade(con, X0=1.0, tau=1.0, noise=None, tmax=10, timestep=0.01):
     Xdot[0] = X0
 
     # 2) RUN THE SIMULATION
-    if noise is None:
-        for t in range(1,nt):
-            Xpre = Xdot[t-1]
-            # Calculate the input to nodes due to couplings
-            xcoup = np.dot(con,Xpre)
-            # Integration step
-            Xdot[t] = Xpre + timestep * ( -1.0*alphas * Xpre + xcoup ) #+ noise[t]
-    else:
-        for t in range(1,nt):
-            Xpre = Xdot[t-1]
-            # Calculate the input to nodes due to couplings
-            xcoup = np.dot(con,Xpre)
-            # Integration step
-            Xdot[t] = Xpre + timestep * ( -1.0*alphas * Xpre + xcoup ) + noise[t]
+    for t in range(1,nt):
+        Xpre = Xdot[t-1]
+        # Calculate the input to nodes due to couplings
+        xcoup = np.dot(con,Xpre)
+        # Integration step
+        Xdot[t] = Xpre + timestep * ( -alphas * Xpre + xcoup ) + noise[t]
 
     return Xdot
 
