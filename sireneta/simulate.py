72799519403d3faffe4cc8b67f7fd8b991ba2f81
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2024-04-17 19:12:41 +0200

    Add 'Sim_' to function names in simulate.py

--- sireneta/simulate.py
+++ sireneta/simulate.py
@@ -23,20 +23,20 @@ models on networks.
 
 Discrete-time models
 ---------------------
-DiscreteCascade
+Sim_DiscreteCascade
     Simulates temporal evolution of the nodes for the discrete cascade.
-RandomWalk
+Sim_RandomWalk
     Simulates temporal evolution of the nodes for the random walks.
 
 Continuous-time models
 -----------------------
-ContCascade
+Sim_ContCascade
     Simulates temporal evolution of the nodes for the continuous cascade.
-LeakyCascade
+Sim_LeakyCascade
     Simulates temporal evolution of the nodes for the leaky-cascade model.
-MOU
+Sim_OrnsteinUhlenbeck
     Simulates temporal evolution for the multivariate Ornstein-Uhlenbeck.
-ContDiffusion
+Sim_ContDiffusion
     Simulates temporal evolution of the nodes for the simple diffustion model.
 
 
@@ -59,7 +59,7 @@ from . import io_helpers
 
 
 ## DISCRETE-TIME CANONICAL MODELS #############################################
-def DiscreteCascade(con, X0=1.0, tmax=10):
+def Sim_DiscreteCascade(con, X0=1.0, tmax=10):
     """Simulates temporal evolution of the nodes for the discrete cascade.
 
     It returns the time-series of the nodes for the discrete cascade model
@@ -109,7 +109,7 @@ def DiscreteCascade(con, X0=1.0, tmax=10):
 
     return Xt
 
-def RandomWalk(con, X0=1.0, tmax=10):
+def Sim_RandomWalk(con, X0=1.0, tmax=10):
     """Simulates temporal evolution of the nodes for the random walks.
 
     It returns the time-series of the nodes for the discrete cascade model
@@ -169,7 +169,7 @@ def RandomWalk(con, X0=1.0, tmax=10):
 
 
 ## CONTINUOUS-TIME CANONICAL MODELS ###########################################
-def ContCascade(con, X0=1.0, noise=None, tmax=10, timestep=0.01):
+def Sim_ContCascade(con, X0=1.0, noise=None, tmax=10, timestep=0.01):
     """Simulates the temporal evolution of the nodes for the continuous cascade.
 
     It solves the differential equation for the simplest possible linear
@@ -254,7 +254,7 @@ def ContCascade(con, X0=1.0, noise=None, tmax=10, timestep=0.01):
 
     return Xdot
 
-def LeakyCascade(con, X0=1.0, tau=1.0, tmax=10, timestep=0.01):
+def Sim_LeakyCascade(con, X0=1.0, tau=1.0, tmax=10, timestep=0.01):
     """Simulates temporal evolution of the nodes for the leaky-cascade model.
 
     It solves the differential equation for the linear propagation model of
@@ -336,7 +336,7 @@ def LeakyCascade(con, X0=1.0, tau=1.0, tmax=10, timestep=0.01):
 
     return Xdot
 
-def MOU(con, X0=1.0, tau=1.0, noise=1.0, tmax=10, timestep=0.01):
+def Sim_OrnsteinUhlenbeck(con, X0=1.0, tau=1.0, noise=1.0, tmax=10, timestep=0.01):
     """Simulates temporal evolution for the multivariate Ornstein-Uhlenbeck.
 
     It solves the differential equation for the linear propagation model of
@@ -426,7 +426,7 @@ def MOU(con, X0=1.0, tau=1.0, noise=1.0, tmax=10, timestep=0.01):
 
     return Xdot
 
-def ContDiffusion(con, X0=1.0, alpha=1.0, noise=None, tmax=10, timestep=0.01):
+def Sim_ContDiffusion(con, X0=1.0, alpha=1.0, noise=None, tmax=10, timestep=0.01):
     """Simulates the temporal evolution of the nodes for the continuous diffusion.
 
     It solves the differential equation for the simplest possible linear
