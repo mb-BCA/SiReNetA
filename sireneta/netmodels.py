17890ca5398429727436490046b343b3dc5153fd
Gorka Zamora-López <gorka@Zamora-Lopez.xyz>
2025-10-31 17:52:02 +0100

    Revised and validated SeedRandomWeights()

--- sireneta/netmodels.py
+++ sireneta/netmodels.py
@@ -55,12 +55,11 @@ ShuffleLinks
 """
 
 # Standard library imports
-
+import warnings
 # Third party packages
 import numpy as np
-# import galib.models
-#from numba import jit
-
+# Local imports from sireneta
+from . import io_helpers
 
 
 ## RANDOM NETWORK MODELS #######################################################
@@ -121,17 +120,23 @@ def GenRandomWeightedCon(N, con_prob, w_distr, directed=True, **arg_w_distr):
 
     return con
 
-def GenRandomMaskNet(mask_con, w_distr, **arg_w_distr):
+def SeedRandomWeights(con, w_distr, **arg_w_distr):
+    # TODO: This function could/should identify whether 'con' is (un)directed and
+    # return (a)symmetric weights accordingly.
     """
-    Generates a squared connectivity matrix for a mask that determines the
-    connectivity topology for the network. Weights are sampled from a given
-    distribution (like numpy.random.uniform or scipy.stats.uniform,
-    scipy.stats.norm).
+    Assigns random weights to the links of a given connectivity matrix. The weights
+    are sampled from a given distribution, e.g., numpy.random.uniform,
+    scipy.stats.uniform or scipy.stats.norm.
+
+    NOTE: The function does not change the input 'con' in place, it returns a
+    copy of 'con' with the new weights.
 
     Parameters
     ----------
-    mask_con : ndarray of shape (N x N).
-        The mask of existing connections.
+    con : ndarray (2d) of shape (N,N).
+        The matrix of a network whose weights want to be randomly assigned.
+        'con' can be either a connectivity matrix (already weighted or not), or
+        the mask (boolean matrix) of an existing 'con'.
     w_distr : function.
         The distribution function for drawing weight samples, it must have a
         'size' argument for the number of generated samples.
@@ -140,23 +145,30 @@ def GenRandomMaskNet(mask_con, w_distr, **arg_w_distr):
 
     Returns
     -------
-    con : ndarray of rank-2 and shape (N x N).
-        A connectivity matrix with topology determined by 'mask_con' and random
-        weights drawn from the distribution 'w_distr'.
+    con : ndarray (2d) of shape (N,N).
+        A connectivity matrix with same links as input 'con' but link weights
+        reassigned, drawn from distribution 'w_distr'.
     """
     # 0) SECURITY CHECKS
-    if (not type(mask_con) == np.ndarray) and len(mask_con.shape) == 2:
-        raise TypeError( "Please enter the matrix shape as integer." )
-    mask_con = np.array(mask_con, dtype=bool)
-    if np.any(mask_con.diagonal()):
-        print( "Warning: Diagonal elements in mask_con ignored." )
-        np.fill_diagonal(mask_con, False)
+    io_helpers.validate_con(con)
 
-    # 1) POPULATE WITH WEIGHTS
-    con = np.zeros_like(mask_con, dtype=float)
-    con[mask_con] = w_distr(**arg_w_distr, size=mask_con.sum())
+    # Extract the mask (if needed)
+    if con.dtype == np.bool:
+        mask = con.copy()
+    else:
+        mask = con.astype(bool)
 
-    return con
+    # Remove self-loops (if needed)
+    if np.any(mask.diagonal()):
+        warnings.warn( "Diagonal elements in 'con' are being ignored.",
+                        category=RuntimeWarning )
+        np.fill_diagonal(mask, False)
+
+    # 1) Create a copy of the matrix and seed the weights to the links
+    newcon = np.zeros_like(mask, dtype=np.float64)
+    newcon[mask] = w_distr(**arg_w_distr, size=mask.sum())
+
+    return newcon
 
 def RndNonNormalNet(con):
     raise ValueError( "Not implemented yet" )
