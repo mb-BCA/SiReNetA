ab5ad58138753d2ce6818e7159907dad7149a32b
Gorka Zamora-López <gorka@Zamora-Lopez.xyz>
2025-12-01 17:08:36 +0100

    Revert "Copyright update"
    This reverts commit a0282b206166361e2cebd6670a04deb737237151.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 
+
 with open("requirements.txt") as reqs_file:
     REQS = [line.rstrip() for line in reqs_file.readlines() if line[0] not in ['\n', '-', '#']]
 
